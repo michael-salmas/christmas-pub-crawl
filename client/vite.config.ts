1cf28ed0a5ff4584179980809007209eb1409a97
michael-salmas <mpsalmas@gmail.com>
2025-11-24 20:36:37 -0700

    configs

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  base: '/christmas-pub-crawl/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
